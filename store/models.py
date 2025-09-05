51c3f5ccc691f9a1e69f3fc4ee7ee92a12abb3fb
Lehlohonolo Mtshizana <lehlohonolomtshizana@icloud.com>
2025-09-05 11:24:49 +0200

    Added Order model, viewset, and endpoint for API

--- store/models.py
+++ store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField(blank=True, null=True)
@@ -26,3 +27,12 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Order of {self.quantity} x {self.product.name}"
--- store/urls.py
+++ store/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import CategoryViewSet, ProductViewSet
+from .views import CategoryViewSet, ProductViewSet, OrderViewSet
 
 router = routers.DefaultRouter()
 router.register(r'categories', CategoryViewSet)
 router.register(r'products', ProductViewSet)
-
+router.register(r'orders', OrderViewSet)
 urlpatterns = [
     path('', include(router.urls)),
 ]
